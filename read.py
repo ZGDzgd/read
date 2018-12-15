d5e70c848b81485621f42fb5a228475fe3d687e5
hello3o <18825987809.com>
2018-12-15 14:59:45 +0800

    Update read.py

--- read.py
+++ read.py
@@ -1,13 +1,23 @@
 import zipfile 
 import re 
+from __important__ import *
 class docx():
-    def __init__(self,file):
+    def __init__(self,file,mode):
         self.__file=file
     def read(self):                 ##self==file
         f=zipfile.ZipFile(self.__file)
         word=f.read("word/document.xml")
         word=re.findall(re.compile("<w:t>.*?</w:t>"),word.decode("utf-8"))
-        for i in range(0,len(word)):
-            word[i]=word[i].replace("<w:t>","")
-            word[i]=word[i].replace("</w:t>","\n")
-        return word
+        if self.__mode=='w':			##only read words
+                word=re.findall(re.compile("<w:t>.*?</w:t>"),word.decode("utf-8"))
+                for i in range(0,len(word)):
+                    word[i]=word[i].replace("<w:t>","")
+                    word[i]=word[i].replace("</w:t>","\n")
+                return word
+        if self.__mode=='p':				##only read pictures
+                f=zipfile.ZipFile(self.__file)
+                filelist=str(f.filelist)
+                filename=re.findall("word/media/image1.*?'",filelist)
+                pictures=f.read(filename[0].replace("'",""))
+                return pictures
+        
