d29549e4e84f384c34cc1e1c83dbc014201d597b
hello3o <18825987809.com>
2018-12-15 14:59:57 +0800

    Update read.py

--- read.py
+++ read.py
@@ -1,13 +1,11 @@
-import zipfile 
-import re 
 from __important__ import *
 class docx():
     def __init__(self,file,mode):
         self.__file=file
+        self.__mode=mode
     def read(self):                 ##self==file
         f=zipfile.ZipFile(self.__file)
         word=f.read("word/document.xml")
-        word=re.findall(re.compile("<w:t>.*?</w:t>"),word.decode("utf-8"))
         if self.__mode=='w':			##only read words
                 word=re.findall(re.compile("<w:t>.*?</w:t>"),word.decode("utf-8"))
                 for i in range(0,len(word)):
